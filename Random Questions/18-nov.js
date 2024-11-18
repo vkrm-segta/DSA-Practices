feb7f76167c87f26e643e43adbe61edd97835f5a
Vikram Segta <vikramsegta43@gmail.com>
2024-11-19 00:01:06 +0530

    fix

---
+++ Random Questions/18-nov.js
@@ -0,0 +1,7 @@
+// 1. What are the three phases of event propagation?
+A: Target > Capturing > Bubbling
+B: Bubbling > Target > Capturing
+C: Target > Bubbling > Capturing
+D: Capturing > Target > Bubbling
+
+// Answer: D
